f6d8de16d425cd227bbedf96f53d0cbe07a00acc
Chris Natali <chris.natali@gmail.com>
2012-07-14 12:35:33 -0400

    expand path of chef files

--- solo.rb
+++ solo.rb
@@ -1,3 +1,3 @@
-file_cache_path "~/networkplanner-devops/chef-repo"
-cookbook_path "~/networkplanner-devops/chef-repo/cookbooks"
-role_path "~/networkplanner-devops/chef-repo/roles"
+file_cache_path File.expand_path("~/networkplanner-devops/chef-repo")
+cookbook_path File.expand_path("~/networkplanner-devops/chef-repo/cookbooks")
+role_path File.expand_path("~/networkplanner-devops/chef-repo/roles")

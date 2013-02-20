1d0d142cb92c71b731c70813aecfefbe58e363b7
chrisnatali <chris.natali@gmail.com>
2013-02-20 12:12:15 -0500

    remove sudo since chef-solo is execd as sudo and reference python-pyproj instead of proj since proj is unavailable in debian repo

--- chef-repo/cookbooks/np_setup/recipes/default.rb
+++ chef-repo/cookbooks/np_setup/recipes/default.rb
@@ -41,15 +41,15 @@ bash "install_libspatialindex" do
     git clone git://github.com/chrisnatali/libspatialindex.git
     apt-get install -y automake libtool
     (cd libspatialindex && ./autogen.sh && ./configure && make && make install)
-    sudo ldconfig
+    ldconfig
     EOH
 end
 
 # And the rest
 bash "install-system-packages" do
     code <<-EOH
-    sudo apt-get install -y python-setuptools libgdal-dev proj python-pip zlibc python-virtualenv python-dev python-numpy python-matplotlib python-gdal python-scipy 
-    sudo ldconfig
+    apt-get install -y python-setuptools libgdal-dev python-pyproj python-pip zlibc python-virtualenv python-dev python-numpy python-matplotlib python-gdal python-scipy 
+    ldconfig
     EOH
 end
 
